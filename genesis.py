5054dea7ad653eeb8c696e859c02ea488bacf279
Mark Zwart <mark.zwart@pobox.com>
2025-09-17 22:56:54 +0200

    Added feedback

--- genesis.py
+++ genesis.py
@@ -4,6 +4,9 @@ from pathlib import Path
 
 BOLD_GREEN = "\x1b[1;32m"
 BOLD_RED = "\x1b[1;31m"
+BOLD_BLUE = "\x1b[1;34m"
+UNDERLINE = "\x1b[4m"
+BOLD_YELLOW = "\x1b[1;33m"
 RESET = "\x1b[0m"
 
 def main():
@@ -31,8 +34,26 @@ def main():
     )
     args = parser.parse_args()
 
-    print(f"{BOLD_GREEN}Afgerond zonder fouten.{RESET}", file=sys.stdout)
+    lst_answers_yes = ["", "J", "JA", "JAWOHL", "Y", "YES"]
+    lst_answers_no = ["N", "NEE", "NEIN", "NO"]
+    while True:
+        msg = f"{BOLD_YELLOW}Waarschuwingen gevonden, wil je doorgaan met? (J/n):{RESET}"
+        print(msg, file=sys.stdout)
+        answer = input(msg)
+        if answer.upper() in lst_answers_no:
+            print(
+                f"{BOLD_RED}'We gaan niet door!{RESET}",
+                file=sys.stdout,
+            )
+        elif answer.upper() in lst_answers_yes:
+            break
+        else:
+            print(
+                f"{BOLD_RED}'{answer}' behoort niet tot de mogelijke antwoorden (j/n).{RESET}",
+                file=sys.stdout,
+            )
 
+    print(f"{BOLD_GREEN}Afgerond zonder fouten.{RESET}", file=sys.stdout)
 
 if __name__ == "__main__":
     main()
