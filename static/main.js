441cddf8cf2d60ba1228c7d0c663ed357046551c
Mark Zwart <mark.zwart@pobox.com>
2025-09-18 23:37:14 +0200

    Make sure progress bar doesn't move to a new line

--- static/main.js
+++ static/main.js
@@ -2,10 +2,21 @@ const consoleBox = document.getElementById("console");
 const evtSource = new EventSource("/stream");
 
 evtSource.onmessage = function(event) {
-    consoleBox.innerHTML += event.data + "<br/>";
-    consoleBox.scrollTop = consoleBox.scrollHeight;
-};
+        const line = event.data;
 
+        // tqdm updates (met carriage returns) → vervang laatste regel i.p.v. toevoegen
+        if (line.includes("%|")) {  // voorbeeld: voortgang
+            // overschrijf laatste regel
+            let lines = consoleBox.innerHTML.split("<br>");
+            lines[lines.length - 1] = line;
+            consoleBox.innerHTML = lines.join("<br>");
+        } else {
+            // gewone nieuwe regel toevoegen
+            consoleBox.innerHTML += line + "<br>";
+        }
+
+        consoleBox.scrollTop = consoleBox.scrollHeight;
+    };
 /**
  * Sends user input to the server via a POST request.
  * This function transmits the provided value as JSON to the "/input" endpoint.
