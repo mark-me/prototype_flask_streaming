1b855c7aa3102a8892826c199940ba5926bb0ef2
Mark M. Zwart <m_zwart@douane.nl>
2025-09-18 11:28:38 +0200

    Added Docstring

--- static/main.js
+++ static/main.js
@@ -6,6 +6,13 @@ evtSource.onmessage = function(event) {
     consoleBox.scrollTop = consoleBox.scrollHeight;
 };
 
+/**
+ * Sends user input to the server via a POST request.
+ * This function transmits the provided value as JSON to the "/input" endpoint.
+ *
+ * Args:
+ *   value: The input value to be sent to the server.
+ */
 function sendInput(value) {
     fetch("/input", {
         method: "POST",
